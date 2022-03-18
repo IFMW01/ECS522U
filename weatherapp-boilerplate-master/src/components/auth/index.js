a32299ac2c6a1e3e935d46b3fd4250bd14dc64b6
IFMW01 <55697240+IFMW01@users.noreply.github.com>
2022-03-18 23:59:57 +0000

    Adding further JavaScript rip to the login screen
    See styling and if-else staircase

--- weatherapp-boilerplate-master/src/components/auth/index.js
+++ weatherapp-boilerplate-master/src/components/auth/index.js
@@ -1,11 +1,37 @@
 import { h, render, Component } from 'preact';
+import React, { useState } from "preact";
 import authstyle from './auth_style_iphone.less'
 
 export default class Auth extends Component {
 
 	// rendering a function when the button is clicked
 	reroute(event){
-		window.location.href='/loading';
+		var userName = document.forms["login"]["uName"].value;
+		var password = document.forms["login"]["PWD"].value;
+		const passwordElement = document.getElementById("PWD");
+		const uNameElement = document.getElementById("uName");
+		console.log(password)
+		if(userName =='' || password ==''){
+			document.getElementById("invalid").style.display = 'block'
+			uNameElement.style.backgroundColor = 'rgb(247, 160, 160)';
+			passwordElement.style.backgroundColor = 'rgb(247, 160, 160)';
+			
+		}
+		else if(userName=='israel' && password == '123456'){
+			document.getElementById("invalid").style.display = 'none';
+			document.getElementById("valid").style.display = 'block'
+
+			passwordElement.style.backgroundColor = 'rgb(160, 247, 167)';
+			uNameElement.style.backgroundColor = 'rgb(247, 160, 160)';
+			setTimeout(() => { window.location.href='/loading'; }, 500);
+			
+			
+		}
+		else{
+			document.getElementById("invalid").style.display = 'block';
+			document.getElementById("PWD").style.backgroundColor = rgb(247,160,160);
+			document.getElementById('uName').style.backgroundColor = rgb(247, 160, 160);
+		}
 		event.preventDefault();
 	}
 	render() {
@@ -19,10 +45,16 @@ export default class Auth extends Component {
 		}
 		return (
             <div>
-				<form onsubmit={this.reroute}>
+				<form onsubmit={this.reroute} name = 'login'>
+							<div id='invalid'class={authstyle.invalid}>
+								<p>Invalid credentaials</p>
+							</div>
+							<div id='valid'class={authstyle.valid}>
+								<p>Valid Credentaials</p>
+							</div>
 					<div class = {authstyle.authInput}>
-							<input type ='text' placeholder='EXAMPLE@EMAIL.COM'></input>
-							<input type ='password' placeholder='PASSWORD'></input>
+							<input id= "uName" type ='text' placeholder='EXAMPLE@EMAIL.COM' name='uName'></input>
+							<input  id= "PWD" type ='password' placeholder='PASSWORD' name ='PWD'></input>
 					</div>
 					<div class={authstyle.authSubmit}>
 							<input type = 'submit'  value ='Login'/>
