0b396e9fda7ef1bc78f7e6163c2628b8904d4444
iamgibby <71736174+iamgibby@users.noreply.github.com>
2022-03-21 21:44:35 +0000

    hourly weather updates

--- weatherapp-boilerplate-master/src/components/hourStats/index.js
+++ weatherapp-boilerplate-master/src/components/hourStats/index.js
@@ -32,7 +32,13 @@ export default class HourStats extends Component {
     }
 
     fetchHourlyTemps = () => {
-        return [Math.round(this.state.temp), Math.round(this.state.temp) + 1, Math.round(this.state.temp) + 2, Math.round(this.state.temp) + 2];
+        var today = new Date();
+        var time = today.getHours();
+        if (time > 18) {
+            return [Math.round(this.state.temp), Math.round(this.state.temp) - 2, Math.round(this.state.temp) - 3, Math.round(this.state.temp) - 3];    
+        } else {
+            return [Math.round(this.state.temp), Math.round(this.state.temp) + 1, Math.round(this.state.temp) + 2, Math.round(this.state.temp) + 2];
+        }
     }
     
     HourlyWeatherIMG = (cond2) => {
@@ -49,6 +55,15 @@ export default class HourStats extends Component {
         }
     }
 
+    fetchCurrentTime = (t) => {
+        var today = new Date();
+        var time = today.getHours() + t;
+        if (time > 23) {
+            time = time - 24;
+        }
+        return time;
+    }
+
     render() {
         const temp1 = this.fetchHourlyTemps()[0];
         const temp2 = this.fetchHourlyTemps()[1];
@@ -60,11 +75,17 @@ export default class HourStats extends Component {
                     <div id="temp1val" class={statsStyle.hourlyTempText}>
                         {temp1}°
                     </div>
+                    <div id="time" class={statsStyle.time}>
+                        {this.fetchCurrentTime(1)}:00
+                    </div>
                     {this.HourlyWeatherIMG(temp1)}
                 </div>
                 <div id="temp2" class={statsStyle.hourlyTemp}>
                     <div id="temp2val" class={statsStyle.hourlyTempText}>
                         {temp2}°
+                    </div>
+                    <div id="time" class={statsStyle.time}>
+                        {this.fetchCurrentTime(2)}:00
                     </div>
                     {this.HourlyWeatherIMG(temp2)}
                 </div>
@@ -72,11 +93,17 @@ export default class HourStats extends Component {
                     <div id="temp3val"class={statsStyle.hourlyTempText}>
                         {temp3}°
                     </div>
+                    <div id="time" class={statsStyle.time}>
+                        {this.fetchCurrentTime(3)}:00
+                    </div>
                     {this.HourlyWeatherIMG(temp3)}
                 </div>
                 <div id="temp4" class={statsStyle.hourlyTemp2}>
                     <div id="temp4val"class={statsStyle.hourlyTempText}>
                         {temp4}°
+                    </div>
+                    <div id="time" class={statsStyle.time}>
+                        {this.fetchCurrentTime(4)}:00
                     </div>
                     {this.HourlyWeatherIMG(temp4)}
                 </div>
