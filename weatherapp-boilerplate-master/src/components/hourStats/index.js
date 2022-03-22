f20cc26e0aa60f0fb4d411ac62ed6f9510a38952
iamgibby <71736174+iamgibby@users.noreply.github.com>
2022-03-22 00:59:03 +0000

    slight changes to hourly times

--- weatherapp-boilerplate-master/src/components/hourStats/index.js
+++ weatherapp-boilerplate-master/src/components/hourStats/index.js
@@ -10,7 +10,8 @@ export default class HourStats extends Component {
 
     state = {
         temp : "",
-        cond : ""
+        cond : "",
+        condM : ""
     }
 
     componentDidMount() {
@@ -20,35 +21,38 @@ export default class HourStats extends Component {
             (data) => {
                 this.setState({
                     temp : data['main']['temp'],
-                    cond : data['weather']['0']['description']
+                    cond : data['weather']['0']['description'],
+                    condM : data['weather']['0']['main']
                 });
         })
 
         this.setState({
 			temp : temperature,
-            cond : conditions
-
+            cond : conditions,
+            condM : conditionsMain
 		}); 
     }
 
     fetchHourlyTemps = () => {
         var today = new Date();
         var time = today.getHours();
-        if (time > 18) {
+        if (time > 18 || time < 2) {
             return [Math.round(this.state.temp), Math.round(this.state.temp) - 2, Math.round(this.state.temp) - 3, Math.round(this.state.temp) - 3];    
+        } else  if (time < 5 && time > 1){
+            return [Math.round(this.state.temp), Math.round(this.state.temp), Math.round(this.state.temp) - 1, Math.round(this.state.temp)];
         } else {
             return [Math.round(this.state.temp), Math.round(this.state.temp) + 1, Math.round(this.state.temp) + 2, Math.round(this.state.temp) + 2];
         }
     }
     
-    HourlyWeatherIMG = (cond2) => {
-        if (cond2 == "Cloudy" || cond2 == "Clear") {
+    HourlyWeatherIMG = (condM) => {
+        if (condM == "Cloudy" || condM == "Clear") {
             return <Icon src = "../../assets/icons/cloudy2.png" width = '45'></Icon>
-        } else if (cond2 == "Sunny") {
+        } else if (condM == "Sunny") {
             return <Icon src = "../../assets/icons/sunny2.png" width = '45'></Icon>
-        } else if (cond2 == "Raining") {
+        } else if (condM == "Raining") {
             return <Icon src = "../../assets/icons/rainy2.png" width = '45'></Icon>
-        } else if (cond2 == "Windy") {
+        } else if (condM == "Windy") {
             return <Icon src = "../../assets/icons/windy2.png" width = '45'></Icon>
         } else {
             return <Icon src = "../../assets/icons/cloudy2.png" width = '45'></Icon>
@@ -57,11 +61,15 @@ export default class HourStats extends Component {
 
     fetchCurrentTime = (t) => {
         var today = new Date();
-        var time = today.getHours() + t;
-        if (time > 23) {
-            time = time - 24;
+        var timeInt = today.getHours() + t;
+        if (timeInt > 23) {
+            timeInt = timeInt - 24;
         }
-        return time;
+        var timeS = timeInt.toString();
+        if (timeInt > 0 && timeInt < 10) {
+            timeS = "0" + timeS;
+        }
+        return timeS;
     }
 
     render() {
@@ -69,11 +77,16 @@ export default class HourStats extends Component {
         const temp2 = this.fetchHourlyTemps()[1];
         const temp3 = this.fetchHourlyTemps()[2];
         const temp4 = this.fetchHourlyTemps()[3];
+        const temps = this.fetchHourlyTemps();
+
+        if (temp1 > 0 && temp1 < 10) {
+            temp1
+        }
         return (
             <div id='hourlyStats' class={ statsStyle.mediaBox}>
                 <div id="temp1" class={statsStyle.hourlyTemp1}>
                     <div id="temp1val" class={statsStyle.hourlyTempText}>
-                        {temp1}°
+                        {temps[0]}°
                     </div>
                     <div id="time" class={statsStyle.time}>
                         {this.fetchCurrentTime(1)}:00
@@ -82,7 +95,7 @@ export default class HourStats extends Component {
                 </div>
                 <div id="temp2" class={statsStyle.hourlyTemp}>
                     <div id="temp2val" class={statsStyle.hourlyTempText}>
-                        {temp2}°
+                        {temps[1]}°
                     </div>
                     <div id="time" class={statsStyle.time}>
                         {this.fetchCurrentTime(2)}:00
@@ -91,7 +104,7 @@ export default class HourStats extends Component {
                 </div>
                 <div id="temp3" class={statsStyle.hourlyTemp}>
                     <div id="temp3val"class={statsStyle.hourlyTempText}>
-                        {temp3}°
+                        {temps[2]}°
                     </div>
                     <div id="time" class={statsStyle.time}>
                         {this.fetchCurrentTime(3)}:00
@@ -100,7 +113,7 @@ export default class HourStats extends Component {
                 </div>
                 <div id="temp4" class={statsStyle.hourlyTemp2}>
                     <div id="temp4val"class={statsStyle.hourlyTempText}>
-                        {temp4}°
+                        {temps[3]}°
                     </div>
                     <div id="time" class={statsStyle.time}>
                         {this.fetchCurrentTime(4)}:00
